a64966ba9e6b398452dc62370e71783007bd99d1
Alina <alina.d@superhow.org>
2023-12-06 16:32:06 +0200

    feat: deployment hardhat settings

--- packages/token/hardhat.config.js
+++ packages/token/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.20",
+  etherscan: {
+    apiKey: process.env.SEPOLIA_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_HOST,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
