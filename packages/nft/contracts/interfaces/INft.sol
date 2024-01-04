5f6874d91eb5f532da9815c6ff0e1974769ea84b
Benas Volkovas <benasvol@gmail.com>
2024-01-04 14:40:11 +0200

    refactor: updated NFT contract

--- packages/nft/contracts/interfaces/INft.sol
+++ packages/nft/contracts/interfaces/INft.sol
@@ -1,30 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
 interface INftEvents {
     /*//////////////////////////////////////////////////////////////////////////
                                        EVENTS
     //////////////////////////////////////////////////////////////////////////*/
 
     event BandMinted(
-        address indexed sender,
+        address indexed receiver,
         uint256 indexed tokenId,
         uint16 level,
         bool isGenesis
     );
 
-    event BandChanged(
+    event BandUpdated(
         address indexed owner,
         uint256 indexed tokenId,
         uint16 oldLevel,
         uint16 newLevel
     );
 
-    event PurchasePaid(address tokenAddress, uint256 amount);
+    event TokensWithdrawn(IERC20 token, address receiver, uint256 amount);
 
-    event RefundPaid(address tokenAddress, uint256 amount);
+    event MaxLevelSet(uint16 newMaxLevel);
 
-    event TokensWithdrawn(address token, address to, uint256 amount);
+    event LevelPriceSet(uint16 level, uint256 price);
+
+    event PurchasePaid(IERC20 token, uint256 amount);
+
+    event RefundPaid(IERC20 token, uint256 amount);
 }
 
 interface INft is INftEvents {
@@ -52,4 +58,47 @@ interface INft is INftEvents {
     error Nft__NonExistantPayment();
     error Nft__PassedZeroAmount();
     error Nft__ZeroAddress();
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                      FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function initialize(
+        string memory name,
+        string memory symbol,
+        IERC20 tokenUSDT,
+        IERC20 tokenUSDC
+    ) external;
+
+    function mintBand(uint16 level, IERC20 token) external;
+
+    function updateBand(
+        uint256 tokenId,
+        uint16 newLevel,
+        IERC20 token
+    ) external;
+
+    function withdrawTokens(IERC20 token, uint256 amount) external;
+
+    function setMaxLevel(uint16 maxLevel) external;
+
+    function setLevelPrice(uint16 level, uint256 price) external;
+
+    function mintGenesisBand(
+        address receiver,
+        uint16 level,
+        uint16 amount
+    ) external;
+
+    function getTokenUSDT() external view returns (IERC20);
+
+    function getTokenUSDC() external view returns (IERC20);
+
+    function getBand(uint256 tokenId) external view returns (Band memory);
+
+    function getLevelPriceInUSD(uint16 level) external view returns (uint256);
+
+    function getMaxLevel() external view returns (uint16);
+
+    function getNextTokenId() external view returns (uint256);
 }
