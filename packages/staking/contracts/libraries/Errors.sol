1f228e1102d9b352f0d0785588bb43d0da8172da
AlinaDiadickina <diadickinaalina@gmail.com>
2024-01-30 16:58:00 +0200

    feat: initial storage allocation

---
+++ packages/staking/contracts/libraries/Errors.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+library Errors {
+    /*//////////////////////////////////////////////////////////////////////////
+                                    STAKING
+    //////////////////////////////////////////////////////////////////////////*/
+
+    error Staking__ZeroAddress();
+    error Staking__ZeroAmount();
+}
