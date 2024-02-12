96771b9e0ddaf9f58b24e9037dc1b3628af1098e
Matas Vosylius <vosyliusmatas@gmail.com>
2024-02-12 16:32:08 +0200

    feat: added get init function and schema

---
+++ packages/subgraph/src/helpers/staking.helpers.ts
@@ -0,0 +1,105 @@
+import { Address, BigInt, log, Bytes } from "@graphprotocol/graph-ts";
+import { BIGINT_ZERO, StakingTypes } from "../utils/constants";
+import {
+    StakingContract,
+    Pool,
+    Band,
+    FundDistribution,
+} from "../../generated/schema"
+import { stringifyStakingType } from "../utils/utils";
+
+
+/**
+ * Retrieves or initializes a StakingContract entity.
+ * @param stakingContractAddress - The address of the stakingContractAddress.
+ * @returns The StakingContract entity.
+ */
+export function getOrInitStakingContract(stakingContractAddress: Address): StakingContract {
+
+    //  @note: redundant ID and vesting contract address.
+    let stakingContractId = "0";
+    let stakingContract = StakingContract.load(stakingContractId);
+
+    if (!stakingContract) {
+
+        stakingContract = new StakingContract(stakingContractId);
+
+        // set default Vesting contract entity values
+        stakingContract.id = "0";
+        stakingContract.stakingContractAddress = stakingContractAddress;
+        stakingContract.totalPools = BIGINT_ZERO;
+        stakingContract.totalBands = BIGINT_ZERO;
+
+        stakingContract.save();
+    }
+
+    return stakingContract;
+}
+
+
+export function getOrInitPool(poolId: BigInt): Pool {
+    
+    let stakingPoolId = poolId.toString();
+    let stakingPool = Pool.load(stakingPoolId);
+
+    if (!stakingPool) {
+
+        stakingPool = new Pool(stakingPoolId);
+
+        // set default Vesting contract entity values
+        stakingPool.id = stakingPoolId;
+        stakingPool.distributionPercentage = BIGINT_ZERO;
+        stakingPool.usdtTokenAmount = BIGINT_ZERO;
+        stakingPool.usdcTokenAmount = BIGINT_ZERO;
+ 
+        stakingPool.save();
+    }
+
+    return stakingPool;
+}
+
+export function getOrInitBand(bandId: BigInt): Band {
+    
+    let stakingBandId = bandId.toString();
+    let stakingBand = Band.load(stakingBandId);
+
+    if (!stakingBand) {
+
+        stakingBand = new Band(stakingBandId);
+
+        stakingBand.id = stakingBandId;
+        stakingBand.stakingType = stringifyStakingType(StakingTypes.FIX);
+        stakingBand.bandLevel = BIGINT_ZERO;
+        stakingBand.price = BIGINT_ZERO;
+        stakingBand.owner = Address.empty();
+        stakingBand.startingSharesAmount = BIGINT_ZERO;
+        stakingBand.stakingStartTimestamp = BIGINT_ZERO;
+        stakingBand.claimableRewardsAmount = BIGINT_ZERO;
+        stakingBand.usdtRewardsClaimed = BIGINT_ZERO;
+        stakingBand.usdcRewardsClaimed = BIGINT_ZERO;
+ 
+        stakingBand.save();
+    }
+
+    return stakingBand;
+}
+
+export function getOrInitFundDistribution(fundDistributionID: BigInt): FundDistribution {
+    
+    let fundDistributionId = fundDistributionID.toString();
+    let fundDistribution = FundDistribution.load(fundDistributionId);
+
+    if (!fundDistribution) {
+
+        fundDistribution = new FundDistribution(fundDistributionId);
+
+        fundDistribution.id = fundDistributionId;
+        fundDistribution.token = Address.empty();
+        fundDistribution.amount = BIGINT_ZERO;
+        fundDistribution.timestamp = BIGINT_ZERO;
+        
+        fundDistribution.save();
+    }
+
+    return fundDistribution;
+}
--- packages/subgraph/src/utils/constants.ts
+++ packages/subgraph/src/utils/constants.ts
@@ -10,3 +10,9 @@ export enum UnlockType {
     DAILY,
     MONTHLY
 }
+// Fixed - user gets shares inmediately, however stake is locks for that selected period
+// Flexi - user earns shares over the time, but stake and be unlocked anytime
+export enum StakingType {
+    FIX,
+    FLEXI
+}
