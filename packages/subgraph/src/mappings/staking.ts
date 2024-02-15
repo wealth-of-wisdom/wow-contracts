d6737cdb3757c61c7b61174f98ec547869183876
Matas Vosylius <vosyliusmatas@gmail.com>
2024-02-15 17:20:05 +0200

    feat: added band and staked handle

--- packages/subgraph/src/mappings/staking.ts
+++ packages/subgraph/src/mappings/staking.ts
@@ -13,39 +13,35 @@ import {
     TotalBandLevelsAmountSet as TokensClaimedAmountSetEvent,
     TotalPoolAmountSet as TotalPoolAmountSetEvent,
     Unstaked as UnstakedEvent,
-    VestingUserRemoved as VestingUserRemovedEvent
+    VestingUserRemoved as VestingUserRemovedEvent,
 } from "../../generated/Staking/Staking";
+import { StakingContract, Pool, Band, FundDistribution } from "../../generated/schema";
 import {
-    StakingContract,
-    Pool,
-    Band,
-    FundDistribution
-} from "../../generated/schema"
-import { getOrInitBand, getOrInitFundDistribution, getOrInitPool, getOrInitStakingContract } from "../helpers/staking.helpers"
+    getOrInitBand,
+    getOrInitFundDistribution,
+    getOrInitPool,
+    getOrInitStakingContract,
+} from "../helpers/staking.helpers";
 import { BIGINT_ZERO } from "../utils/constants";
 import { stringifyStakingType } from "../utils/utils";
 import { BigInt, store } from "@graphprotocol/graph-ts";
-
 
 /**
  * Handles the Initialized event triggered when the contract is initialized.
  * @param event - The InitializedEvent containing the contract address.
  */
 export function handleInitialized(event: InitializedEvent): void {
-
     const stakingContract: StakingContract = getOrInitStakingContract(event.address);
 
     stakingContract.save();
-
 }
 // Band add
 export function handleBandStaked(event: BandStakedEvent): void {
-
     const band: Band = getOrInitBand(event.params.bandId);
 
     // band.stakingType = event.params
     band.bandLevel = event.params.bandLevel;
-    band.owner = event.params.user
+    band.owner = event.params.user;
     // band.price = event.params.price;
     band.startingSharesAmount = BIGINT_ZERO;
     band.stakingStartTimestamp = event.block.timestamp;
@@ -57,18 +53,26 @@ export function handleBandStaked(event: BandStakedEvent): void {
 }
 // Band remove
 export function handleBandUnstaked(event: BandUnstakedEvent): void {
-
     const band: Band = getOrInitBand(event.params.bandId);
 
-    store.remove("Band", band.id)
+    store.remove("Band", band.id);
 
-    band.save()
+    band.save();
 }
 
 // Band upgrade
 export function handleBandUpgaded(event: BandUpgadedEvent): void {
+    const band: Band = getOrInitBand(event.params.bandId);
 
-    const band: Band = getOrInitBand(event.params.bandId)
+    band.bandLevel = event.params.newBandLevel;
+    band.owner = event.params.user;
+
+    band.save();
+}
+
+// Band downgrade
+export function handleBandDowngraded(event: BandDowngradedEvent): void {
+    const band: Band = getOrInitBand(event.params.bandId);
 
     band.bandLevel = event.params.newBandLevel;
     band.owner = event.params.user;
@@ -77,8 +81,7 @@ export function handleBandUpgaded(event: BandUpgadedEvent): void {
 }
 
 export function handleFundsDistributed(event: FundsDistributedEvent): void {
-
-    const fundsDistribution = getOrInitFundDistribution(event.transaction.hash)
+    const fundsDistribution = getOrInitFundDistribution(event.transaction.hash);
 
     fundsDistribution.amount = event.params.amount;
     fundsDistribution.timestamp = event.block.timestamp;
@@ -86,3 +89,34 @@ export function handleFundsDistributed(event: FundsDistributedEvent): void {
 
     fundsDistribution.save();
 }
+
+export function handleRewardsClaimed(event: RewardsClaimedEvent): void {
+    // @todo This event should also emit band ID, to track claimed rewards from band
+    const band = getOrInitBand(event.params.bandId)
+
+    // @todo add this to constants file
+    const usdtAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+    const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+
+    if (event.params.token.toString() === usdtAddress) {
+        band.usdtRewardsClaimed = event.params.totalRewards;
+    } else if (event.params.token.toString() === usdcAddress) {
+        band.usdcRewardsClaimed = event.params.totalRewards;
+    }
+
+    band.save();
+}
+
+// @note What is the difference between StakedEvent and handleBandStaked???
+export function handleStaked(event: StakedEvent): void {
+   
+    // @todo This event should also emit band ID, to track staked band
+    const band = getOrInitBand(event.params.bandId)
+
+    band.bandLevel = event.params.bandLevel;
+    band.stakingType = event.params.stakingType;
+    band.owner = event.params.user
+    
+    band.save();
+}
+
