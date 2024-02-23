6a87313cca3c0cf36576019391dd70a9bc71c6a3
AlinaDiadickina <diadickinaalina@gmail.com>
2024-02-23 14:01:23 +0200

    feat: deploy staking script

---
+++ packages/staking/scripts/deployStaking.js
@@ -0,0 +1,20 @@
+const { ethers, upgrades } = require("hardhat")
+require("dotenv").config()
+
+async function main() {
+    const Staking = await ethers.getContractFactory("Staking")
+    const staking = await upgrades.deployProxy(Staking, [
+        process.env.USDT_TOKEN,
+        process.env.USDC_TOKEN,
+        process.env.WOW_TOKEN,
+        process.env.TOTAL_POOLS,
+        process.env.TOTAL_BAND_LEVELS,
+    ])
+    await sale.waitForDeployment()
+    console.log("Staking deployed to:", await staking.getAddress())
+}
+
+main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
