1bcfb0f2b21f8d2d74faeaa7f3dbf58de07918a2
Benas Volkovas <benasvol@gmail.com>
2024-04-10 12:25:57 +0300

    feat: updated constants

--- packages/subgraph/tests/utils/data/dates.ts
+++ packages/subgraph/tests/utils/data/dates.ts
@@ -9,31 +9,31 @@ export const monthInSeconds: BigInt = BigInt.fromI32(30 * 24 * 60 * 60);
 export const preInitDate: BigInt = BigInt.fromI32(1000000000); // Sun Sep 09, 2001 1:46:40 AM
 export const initDate: BigInt = BigInt.fromI32(1710000000); // Sat Mar 09 2024 16:00:00 GMT+0000
 
-export const month1AfterInit: BigInt = initDate.plus(monthInSeconds);
-export const month2AfterInit: BigInt = month1AfterInit.plus(monthInSeconds);
-export const month3AfterInit: BigInt = month2AfterInit.plus(monthInSeconds);
-export const month4AfterInit: BigInt = month3AfterInit.plus(monthInSeconds);
-export const month5AfterInit: BigInt = month4AfterInit.plus(monthInSeconds);
-export const month6AfterInit: BigInt = month5AfterInit.plus(monthInSeconds);
-export const month7AfterInit: BigInt = month6AfterInit.plus(monthInSeconds);
-export const month8AfterInit: BigInt = month7AfterInit.plus(monthInSeconds);
-export const month9AfterInit: BigInt = month8AfterInit.plus(monthInSeconds);
-export const month10AfterInit: BigInt = month9AfterInit.plus(monthInSeconds);
-export const month11AfterInit: BigInt = month10AfterInit.plus(monthInSeconds);
-export const month12AfterInit: BigInt = month11AfterInit.plus(monthInSeconds);
-
 export const monthsAfterInit: BigInt[] = [
     initDate,
-    month1AfterInit,
-    month2AfterInit,
-    month3AfterInit,
-    month4AfterInit,
-    month5AfterInit,
-    month6AfterInit,
-    month7AfterInit,
-    month8AfterInit,
-    month9AfterInit,
-    month10AfterInit,
-    month11AfterInit,
-    month12AfterInit,
+    initDate.plus(monthInSeconds),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(2))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(3))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(4))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(5))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(6))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(7))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(8))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(9))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(10))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(11))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(12))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(13))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(14))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(15))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(16))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(17))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(18))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(19))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(20))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(21))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(22))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(23))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(24))),
+    initDate.plus(monthInSeconds.times(BigInt.fromI32(25))),
 ];
