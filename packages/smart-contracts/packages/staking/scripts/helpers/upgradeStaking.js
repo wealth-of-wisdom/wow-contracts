e9516f43d9dbaf4793468d0cfdea7fae052500c8
Benas Volkovas <benasvol@gmail.com>
2024-04-30 11:19:57 +0300

    feat: updated upgrade staking scripts

--- packages/smart-contracts/packages/staking/scripts/helpers/upgradeStaking.js
+++ packages/smart-contracts/packages/staking/scripts/helpers/upgradeStaking.js
@@ -1,9 +1,19 @@
-const { ethers, upgrades } = require("hardhat")
+const { ethers, upgrades, network } = require("hardhat")
+const { TESTNET_NETWORKS, MAINNET_NETWORKS } = require("./constants")
 
-async function upgradeStaking(stakingProxyAddress, stakingContractName) {
+async function upgradeStaking(stakingProxyAddress) {
     /*//////////////////////////////////////////////////////////////////////////
                                   UPGRADE STAKING
     //////////////////////////////////////////////////////////////////////////*/
+
+    let stakingContractName = ""
+    if (TESTNET_NETWORKS.includes(network.name)) {
+        stakingContractName = "StakingMock"
+    } else if (MAINNET_NETWORKS.includes(network.name)) {
+        stakingContractName = "Staking"
+    } else {
+        throw new Error("Network not supported")
+    }
 
     const Staking = await ethers.getContractFactory(stakingContractName)
     const staking = await upgrades.upgradeProxy(stakingProxyAddress, Staking)
--- packages/smart-contracts/packages/staking/scripts/upgradeStaking.js
+++ packages/smart-contracts/packages/staking/scripts/upgradeStaking.js
@@ -1,34 +1,16 @@
-<<<<<<< HEAD
-const { network } = require("hardhat")
-=======
-const upgradeStaking = require("./helpers/upgradeStaking")
-const deployStakingImplementation = require("./helpers/deployStakingImplementation")
->>>>>>> fix/staking-verification
-require("dotenv").config()
 const upgradeStaking = require("./helpers/upgradeStaking")
 const verifyStaking = require("./helpers/verifyStaking")
+require("dotenv").config()
 
 async function main() {
-    /*//////////////////////////////////////////////////////////////////////////
-                                CHECK NETWORK
-    //////////////////////////////////////////////////////////////////////////*/
-
-    let stakingContractName = ""
-    if (network.name === "sepolia" || network.name === "arbitrumSepolia") {
-        stakingContractName = "StakingMock"
-    } else if (network.name === "ethereum" || network.name === "arbitrumOne") {
-        stakingContractName = "Staking"
-    } else {
-        throw new Error("Network not supported")
-    }
     /*//////////////////////////////////////////////////////////////////////////
                                 UPGRADE STAKING
     //////////////////////////////////////////////////////////////////////////*/
 
-    await upgradeStaking(process.env.STAKING_PROXY_ADDRESS, stakingContractName)
+    await upgradeStaking(process.env.STAKING_PROXY_ADDRESS)
 
     /*//////////////////////////////////////////////////////////////////////////
-                                      VERIFY STAKING
+                                VERIFY STAKING
     //////////////////////////////////////////////////////////////////////////*/
 
     await verifyStaking(process.env.STAKING_PROXY_ADDRESS)
