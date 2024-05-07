5651c093da31a45b3426ebe55ea5ece855814354
Benas Volkovas <benasvol@gmail.com>
2024-05-07 11:12:21 +0300

    fix: updated vesting scripts

--- packages/smart-contracts/packages/vesting/scripts/sub-tasks/addVestingPools.js
+++ packages/smart-contracts/packages/vesting/scripts/sub-tasks/addVestingPools.js
@@ -1,12 +1,20 @@
 const addVestingPools = require("../helpers/addVestingPools")
+const copyTokenContracts = require("../helpers/copyTokenContracts")
+const removeTokenContracts = require("../helpers/removeTokenContracts")
 require("dotenv").config()
 
 async function main() {
+    // Copy contract from token package to allow access to token functions
+    await copyTokenContracts()
+
+    // Add vesting pools
     await addVestingPools(
         process.env.VESTING_TOKEN,
         process.env.VESTING_CONTRACT,
-        process.env.FULL_POOL_TOKEN_AMOUNT,
     )
+
+    // Remove temp folders
+    await removeTokenContracts()
 }
 
 main().catch((error) => {
