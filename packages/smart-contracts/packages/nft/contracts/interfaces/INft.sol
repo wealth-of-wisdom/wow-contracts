4a753f979b3cfccb2363b0535b3b6fde2b5a41f7
AlinaDiadickina <diadickinaalina@gmail.com>
2024-05-21 09:42:35 +0300

    feat: added active nft setter

--- packages/smart-contracts/packages/nft/contracts/interfaces/INft.sol
+++ packages/smart-contracts/packages/nft/contracts/interfaces/INft.sol
@@ -40,6 +40,8 @@ interface INftEvents {
 
     event PromotionalVestingPIDSet(uint16 newPID);
 
+    event ActiveUserNftSet(address user, uint256 newId);
+
     event VestingContractSet(IVesting newContract);
 
     event NextTokenIdSet(uint256 nextTokenId);
