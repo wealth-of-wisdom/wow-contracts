5adc52b878e5876af0d02851baa77ca8cde7d0ac
AlinaDiadickina <diadickinaalina@gmail.com>
2024-06-12 12:52:31 +0300

    feat: upgrade vesting scripts

---
+++ packages/smart-contracts/packages/vesting/scripts/helpers/upgradeVesting.js
@@ -0,0 +1,19 @@
+const { ethers, upgrades } = require("hardhat")
+
+async function upgradeVesting(vestingProxyAddress) {
+    if (!vestingProxyAddress) {
+        throw new Error("ERROR: Vesting proxy address not found")
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                    UPGRADE VESTING
+    //////////////////////////////////////////////////////////////////////////*/
+
+    const Vesting = await ethers.getContractFactory("Vesting")
+    const vesting = await upgrades.upgradeProxy(vestingProxyAddress, Vesting)
+    await vesting.waitForDeployment()
+
+    console.log("Vesting upgraded with proxy address: ", vestingProxyAddress)
+}
+
+module.exports = upgradeVesting
