3517ca39e6bd01d76d3649b55d76c80e50237868
AlinaDiadickina <diadickinaalina@gmail.com>
2024-06-13 12:16:54 +0300

    feat: added new data for token uri changes

--- packages/smart-contracts/packages/nft/scripts/helpers/updateTokenUris.js
+++ packages/smart-contracts/packages/nft/scripts/helpers/updateTokenUris.js
@@ -1,38 +1,39 @@
-const { ethers } = require("hardhat");
-const tokenData = require("../data/tokenUpdateData.json");
+const { ethers } = require("hardhat")
+const tokenData = require("../data/tokenUpdateData.json")
 
 // Pre-load and map levels to URIs
-const levels = tokenData.levels;
+const levels = tokenData.levels
 const levelToUriMap = levels.reduce((map, levelInfo) => {
-    map[levelInfo.level] = levelInfo.uri;
-    return map;
-}, {});
+    map[levelInfo.level] = levelInfo.uri
+    return map
+}, {})
 
 async function updateTokenUris(nftAddress) {
-
-    const Nft = await ethers.getContractFactory("Nft");
-    const nft = Nft.attach(nftAddress);
+    const Nft = await ethers.getContractFactory("Nft")
+    const nft = Nft.attach(nftAddress)
 
     /*//////////////////////////////////////////////////////////////////////////
                                     CHANGE TOKEN URI
     //////////////////////////////////////////////////////////////////////////*/
 
     for (let token of tokenData.tokens) {
+        const baseUri = levelToUriMap[token.level]
+        const fullUri = `${baseUri}/${token.idInLevel}.json`
 
-
-        const baseUri = levelToUriMap[token.level];
-        const fullUri = `${baseUri}/${token.idInLevel}.json`;
-
-        console.log(`Updating token ID ${token.id} at level ${token.level} to new URI ${fullUri}`);
+        console.log(
+            `Updating token ID ${token.id} at level ${token.level} to new URI ${fullUri}`,
+        )
 
         try {
-            const tx = await nft.setTokenURI(token.id, fullUri);
-            await tx.wait();
-            console.log(`Token ID ${token.id} updated to ${fullUri}`);
+            const tx = await nft.setTokenURI(token.id, fullUri)
+            await tx.wait()
+            console.log(`Token ID ${token.id} updated to ${fullUri}`)
         } catch (error) {
-            console.error(`Failed to update token ID ${token.id} with URI ${fullUri}: ${error.message}`);
+            console.error(
+                `Failed to update token ID ${token.id} with URI ${fullUri}: ${error.message}`,
+            )
         }
     }
 }
 
-module.exports = updateTokenUris;
+module.exports = updateTokenUris
